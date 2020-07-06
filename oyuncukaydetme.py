4f8e7c247d821403f098e05db47ffacee28feb3d
Taner Özer <44675799+dxtaner@users.noreply.github.com>
2020-07-06 16:07:00 +0300

    player

---
+++ oyuncukaydetme.py
@@ -0,0 +1,10 @@
+print("Kaydetme")
+
+isim = input("oyuncu adi :")
+takim = input("takim adi :")
+
+veriler=[isim,takim]
+
+print("kaydediliyor")
+
+print("oyuncu adi : {} \noyuncu takimi: {} ".format(veriler[0],veriler[1]))
