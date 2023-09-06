9a12e5eccf6a9a074df1b4a00226e1d813e59bca
taner <tanerozer16@gmail.com>
2023-09-06 14:14:27 +0300

    Create cardPackets2.py

---
+++ cardPackets2.py
@@ -0,0 +1,21 @@
+def cardPackets(cardTypes):
+    cards_required = 0
+
+    for quantity in cardTypes:
+        # Her kart türü için ek kart sayısını hesaplayın ve maksimuma ekleyin
+        additional_cards = quantity % 2
+        cards_required += additional_cards
+
+    # En az iki paket olacak şekilde düzenleme yapın
+    if cards_required % 2 != 0:
+        cards_required += 1
+
+    return cards_required
+
+
+# Örnek giriş
+cardTypes = [3, 9, 7, 6, 5, 2]
+
+# İşlevi çağırın ve sonucu yazdırın
+result = cardPackets(cardTypes)
+print("Minimum ek kart sayısı (en az iki paket):", result)
