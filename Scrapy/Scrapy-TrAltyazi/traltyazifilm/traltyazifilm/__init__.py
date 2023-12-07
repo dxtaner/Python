a6bb8aa2819f60aaf98503c5ae01cfc92f38512c
taner <tanerozer16@gmail.com>
2023-12-07 11:51:39 +0300

    traltyazifilm

---
+++ Scrapy/Scrapy-TrAltyazi/traltyazifilm/traltyazifilm/__init__.py
@@ -0,0 +1,6 @@
+# response.css("td.rowbeyaz a span::text").extract() film tr adi
+# response.css("td.rowbeyaz span:nth-child(3)::text").extract()  fil orginal adi
+# response.css("td.rowbeyaz div span::text").extract() tra oy sayisi
+# response.css("td.rowbeyaz div span strong::text").extract() tra oy ortalamasi
+# response.css("td.rowbeyaz span:nth-child(2)::text").extract() imdb oy sayisi
+# response.css("td.rowbeyaz span b::text").extract()  imdb oy ortalamasi
